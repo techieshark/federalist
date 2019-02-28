0d0e9ce817a4bdb5679d9e82e9a5b777b3ac03c0
Eddie A Tejeda <eddie.tejeda@gsa.gov>
2019-02-28 16:46:48 -0500

    reintroduce report and clean up language

--- config/templates.js
+++ config/templates.js
@@ -4,17 +4,26 @@ module.exports = {
     repo: 'federalist-uswds-template',
     branch: 'master',
     example: 'https://federalist-proxy.app.cloud.gov/site/18f/federalist-uswds-template/',
-    title: 'U.S. Web Design System v1.0 (Jekyll)',
-    description: 'A flexible multi-page setup intended to introduce an agency or program. It offers an engaging Hero section for a current priority and a landing page layout designed to provide clarity and context for first time visitors. This template also offers navigation options for internal pages.',
+    title: 'U.S. Web Design System v1.6 (Jekyll)',
+    description: 'A multi-page setup intended to introduce an agency or program. It offers an engaging Hero section for a current priority and a landing page layout designed to provide clarity and context for first time visitors. This template also offers navigation options for internal pages. It is designed for sites that do not require heavy customization.',
     thumb: '/images/federalist-uswds-template.jpg',
   },
+  report: {
+    owner: '18f',
+    repo: 'federalist-uswds-template',
+    branch: 'report-config',
+    example: 'https://federalist-proxy.app.cloud.gov/preview/18f/federalist-uswds-template/report-config/',
+    title: 'U.S. Web Design System v1.6 (Jekyll) - Basic Report',
+    description: 'A template designed for publishing reports and documents. Navigation can be customized to support many sections, while maintaining a clear hierarchy.',
+    thumb: '/images/federalist-report-template.jpg',
+  }
   uswds2: {
     owner: '18f',
     repo: 'federalist-uswds-jekyll',
     branch: 'master',
     example: 'https://federalist-proxy.app.cloud.gov/site/18f/federalist-uswds-jekyll/',
     title: 'U.S. Web Design System v2.0-beta (Jekyll)',
-    description: 'A flexible multi-page setup intended to introduce an agency or program. It offers an engaging Hero section for a current priority and a landing page layout designed to provide clarity and context for first time visitors. This template also offers navigation options for internal pages.',
+    description: 'A template designed to help launch a modern mobile-friedly and accessible website using the latest version of the U.S Web Design System. This template is designed to help you get started quickly but also provides a foundation that you use to build out larger and more complex sites.',
     thumb: '/images/federalist-uswds-jekyll.png',
   },
   gatsby: {
@@ -23,7 +32,7 @@ module.exports = {
     branch: 'master',
     example: 'https://federalist-proxy.app.cloud.gov/site/18f/federalist-uswds-gatsby/',
     title: 'U.S. Web Design System v2.0-beta (Gatsby)',
-    description: 'A flexible multi-page setup intended to introduce an agency or program using Gatsby.js. It offers an engaging Hero section for a current priority and a landing page layout designed to provide clarity and context for first time visitors. This template also offers navigation options for internal pages.',
+    description: 'An advanced template designed to help launch a modern mobile-friedly and accessible website using the latest version of the U.S Web Design System. This template is built with Javascript and uses React.js and Gatsby.js and provides a foundation that you use to build out larger and more complex sites. ',
     thumb: '/images/federalist-gatsby-uswds-template.png',
-  },
+  }
 };
